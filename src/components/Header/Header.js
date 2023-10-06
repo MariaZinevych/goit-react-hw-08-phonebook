b2f37a40a38f5955b1c97a74a735ae05bac874b8
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-10-06 21:10:01 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Container, HStack } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import AuthNav from 'components/AuthNavigation/AuthNavigation';
 import Navigation from 'components/Navigation/Navigation';
-
 import useAuth from 'hooks/useAuth';
 import UserMenu from 'components/UserMenu/UserMenu';
 
@@ -10,12 +9,10 @@ const Header = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <HStack as="header" pt={7} pb={7} shadow="md">
-      <Container maxW="1280px" display="flex" alignItems="center">
-        <Navigation />
-        {isLoggedIn ? <UserMenu /> : <AuthNav />}
-      </Container>
-    </HStack>
+    <Container maxW="1280px" display="flex" alignItems="center">
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </Container>
   );
 };
 // qw
